9647a29b23277080a89b3b027b32e1a697e986be
laayounii <meedaziz001@gmail.com>
2023-10-18 09:54:21 +0100

    Smile in the mirror

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main-prints the lowercase alphabet in reverse
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	char alphabet = 'z';
+
+	while (alphabet <= 'a')
+	{
+		putchar(alphabet);
+		alphabet--;
+	}
+	putchar('\n');
+	return (0);
+}
