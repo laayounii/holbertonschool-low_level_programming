5d11ac7b88e390aca43d0324e101bd5ba1bfc48c
laayounii <meedaziz001@gmail.com>
2023-11-01 19:15:33 +0100

    is prime numb

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -9,14 +9,14 @@ int is_prime_number(int n)
 {
 	if (n <= 1)
 	{
-		return (0); // 0 and 1 are not prime numbers
+		return (0);
 	}
 
 	for (int i = 2; i * i <= n; i++)
 	{
 		if (n % i == 0)
 		{
-			return (0); // n is divisible by a number other than 1 and itself, so it's not prime
+			return (0);
 		}
 	}
 
