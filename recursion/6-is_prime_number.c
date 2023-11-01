cba85cb4ca59f210673e000f1757a57d90ba6e12
laayounii <meedaziz001@gmail.com>
2023-11-01 19:22:32 +0100

    is prime numb

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,24 +1,32 @@
 #include "main.h"
 /**
- * is_prime_number - func that returns 1 if a num is a prime num, 0 otherwise
- * @n: number for input
- * @i: for the increment
- * Return: 0
- */
+* is_prime_number - returns true if the number is prime
+*@n: the number to check
+*
+*Return: true if the number is prime
+*/
 int is_prime_number(int n)
 {
+	int start = n / 2;
+
 	if (n <= 1)
-	{
 		return (0);
-	}
+	return (is_prime(n, start));
+}
 
-	for (int i = 2; i * i <= n; i++)
-	{
-		if (n % i == 0)
-		{
-			return (0);
-		}
-	}
+/**
+* is_prime - returns the 1 if n is prime
+* @n: number to be checked
+* @x: number to start checking from
+*
+* Return: 1 if n is prime, 0 otherwise
+*/
 
-	return (1);
+int is_prime(int n, int x)
+{
+	if (x <= 1)
+		return (1);
+	else if (n % x == 0)
+		return (0);
+	return (is_prime(n, x - 1));
 }
