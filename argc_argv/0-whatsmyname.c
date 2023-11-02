5ba06c562a062b6a9df1c54935382d45fbc283b3
laayounii <meedaziz001@gmail.com>
2023-11-02 12:18:05 +0100

    say my name

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -4,10 +4,10 @@
  * @argc: the number of command-line arguments
  * @argv: the array of strings that contains these arguments.
  * Rretur: 0
-*/
+ */
 int main(int argc, char *argv[])
- {
-    (void)argc;
-    printf("%s\n", argv[0]);
-    return (0);
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
 }
