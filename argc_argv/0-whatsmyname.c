ce2b76a9f709b64ebd7f7830315bd9fede555b66
laayounii <meedaziz001@gmail.com>
2023-11-02 12:14:32 +0100

    say my name

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - program that prints its name, followed by a new line.
+ * @argc: the number of command-line arguments
+ * @argv: the array of strings that contains these arguments.
+ * Rretur: 0
+*/
+int main(int argc, char *argv[])
+ {
+    printf("%s\n", argv[0]);
+    return (0);
+}
