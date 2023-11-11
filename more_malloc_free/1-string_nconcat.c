03546d1ed928b46657055ab68e58932d7b9ee51f
laayounii <meedaziz001@gmail.com>
2023-11-11 16:16:26 +0100

    calloc

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,6 +1,6 @@
 #include "main.h"
 #include <stddef.h>
-#include <stddef.h>
+#include <stdlib.h>
 /**
  * string_nconcat - function to concatnate strings with n bytes
  * @s1: destination for concatnation
