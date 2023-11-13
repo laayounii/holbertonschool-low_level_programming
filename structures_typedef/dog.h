c4ed833a2a812095cc6971c2dcf0d6f9dcee215d
laayounii <meedaziz001@gmail.com>
2023-11-13 11:16:18 +0100

    dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,19 @@
+#ifndef __STRUCT__
+#define __STRUCT__
+#include <stdio.h>
+#include <string.h>
+/**
+ * struct dog - frist struct
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ */
 struct dog
 {
 	char *name;
 	char *owner;
 	float age;
+	void init_dog(struct dog *d, char *name, float age, char *owner);
 };
+#endif
