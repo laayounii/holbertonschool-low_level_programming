fdc1925e44dd0ae4035c86c361b20b345451cc49
laayounii <meedaziz001@gmail.com>
2023-11-14 14:07:13 +0100

    dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef dog
-#define dog
+#ifndef __STRUCT__
+#define __STRUCT__
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -10,13 +10,18 @@
  * @owner: Third member
  *
  */
+
 struct dog
 {
 	char *name;
-	char *owner;
 	float age;
+	char *owner;
 };
+
 typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-#endif
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif /*__STRUCT__*/
