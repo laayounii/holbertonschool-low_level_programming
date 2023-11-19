c365dd6d0153f3c617a5e827e6d394d01e6cceb4
laayounii <meedaziz001@gmail.com>
2023-11-19 21:54:33 +0100

    print all

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,79 +1,87 @@
 #include "variadic_functions.h"
-#include <stdlib.h>
-#include <stdio.h>
-
 /**
- * _printchar - print char type element from va_list
- * @list: va_list passed to function
+ * convert_char - print a character
+ * @argm: list of characters arguments
+ *
+ * Return: void
  */
-void _printchar(va_list list)
+void convert_char(va_list argm)
 {
-	printf("%c", va_arg(list, int));
+	printf("%c", va_arg(argm, int));
 }
-
 /**
- * _printstr - print string element from va_list
- * @list: va_list passed to function
+ * convert_int - print an integer
+ * @argm: list of integers arguments
+ *
+ * Return: void
  */
-void _printstr(va_list list)
+void convert_int(va_list argm)
 {
-	char *s;
-
-	s = va_arg(list, char *);
-	if (s == NULL)
-		s = "(nil)";
-	printf("%s", s);
+	printf("%d", va_arg(argm, int));
 }
-
 /**
- * _printfloat - print float type element from va_list
- * @list: va_list passed to function
+ * convert_float - print a float number
+ * @argm: list of float numbers arguments
+ *
+ * Return: void
  */
-void _printfloat(va_list list)
+void convert_float(va_list argm)
 {
-	printf("%f", va_arg(list, double));
+	printf("%f", va_arg(argm, double));
 }
-
 /**
- * _printint - print int type element from va_list
- * @list: va_list passed to function
+ * convert_string - print a string
+ * @argm: list of string arguments
+ *
+ * Return: void
  */
-void _printint(va_list list)
+void convert_string(va_list argm)
 {
-	printf("%d", va_arg(list, int));
+	char *ch;
+
+	ch = va_arg(argm, char *);
+	if (ch == NULL)
+		ch = "(nil)";
+	printf("%s", ch);
 }
-
 /**
- * print_all - print anything passed if char, int, float, or string.
- * @format: string of formats to use and print
+ * print_all - a function that prints anything.
+ * @format: format of arguments
+ * @... : the arguments to print
+ *
+ * Return: void
  */
-void print_all(const char * const format, ...)
+void print_all(const char *const format, ...)
 {
-	unsigned int i, j;
-	va_list args;
-	char *sep;
+	int i = 0;
+	int j;
+	char *separator1 = "";
+	char *separator2 = ", ";
+	va_list argm;
 
-	checker storage[] = {
-		{ "c", _printchar },
-		{ "f", _printfloat },
-		{ "s", _printstr },
-		{ "i", _printint }
+	types_t types[] = {
+		{'c', convert_char},
+		{'i', convert_int},
+		{'f', convert_float},
+		{'s', convert_string},
+		{'\0', NULL},
 	};
-
-	i = 0;
-	sep = "";
-	va_start(args, format);
-	while (format != NULL && format[i / 4] != '\0')
+	va_start(argm, format);
+	while (format && format[i])
 	{
-		j = i % 4;
-		if (storage[j].type[0] == format[i / 4])
+		j = 0;
+		while (types[j].type)
 		{
-			printf("%s", sep);
-			storage[j].f(args);
-			sep = ", ";
+			if (format[i] == types[j].type)
+			{
+				printf("%s", separator1);
+				types[j].function(argm);
+				separator1 = separator2;
+			}
+			j++;
 		}
 		i++;
 	}
 	printf("\n");
-	va_end(args);
+	va_end(argm);
 }
