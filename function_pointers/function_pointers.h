074ff5cda2f26816e4eb3710d3ba6f0efff6cc44
laayounii <meedaziz001@gmail.com>
2023-11-21 09:56:44 +0100

    func ptr

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,7 +1,12 @@
-#ifndef FUNCTION_POINTERS_H
-#define FUNCTION_POINTERS_H
-#include <stddef.h>
+#ifndef __FUNCTION_POINTERS__
+#define __FUNCTION_POINTERS__
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
-#endif
+int (*get_op_func(char *s))(int, int);
+
+#endif /*__FUNCTION_POINTERS__*/
