faaea15907fbef27ec313110897764eb372f3a71
laayounii <meedaziz001@gmail.com>
2023-11-29 13:45:54 +0100

    List length

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -8,13 +8,13 @@
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-    size_t node_count = 0;
+	size_t node_count = 0;
 
-    while (h != NULL)
-    {
-        node_count++;
-        h = h->next;
-    }
+	while (h != NULL)
+	{
+		node_count++;
+		h = h->next;
+	}
 
-    return node_count;
+	return (node_count);
 }
