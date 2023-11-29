5df7b87fda3d94be10ee32c4b63a126090ddff5c
laayounii <meedaziz001@gmail.com>
2023-11-29 11:47:08 +0100

    lists.h

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,2 +1,19 @@
-int _putchar(char c);
+#ifndef LISTS_H
+#define LISTS_H
+#include <stddef.h>
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
 size_t print_list(const list_t *h);
+int _putchar(char c)
+
+#endif
