f6c65b63232bdcaea82dab646d11ea1a34e7ab23
laayounii <meedaziz001@gmail.com>
2023-11-29 11:59:54 +0100

    lists.h

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -14,9 +14,9 @@
  */
 typedef struct list_s
 {
-    char *str;
-    size_t len;
-    struct list_s *next;
+	char *str;
+	size_t len;
+	struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
