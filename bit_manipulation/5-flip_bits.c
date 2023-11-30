58be4c2fc53305d926e714749f3fef5c639abf21
laayounii <meedaziz001@gmail.com>
2023-11-30 13:30:38 +0100

    flip bits

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * flip_bits - returns the number of bits needed to flip to get from one number to another
+ * flip_bits - returns the number of bits needed to flip
  * @n: first number
  * @m: second number
  *
